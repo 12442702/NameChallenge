4173bc95102cd6465c0ed6c7260fe02207198817
Peter <LiX196@cardiff.ac.uk>
2023-06-21 10:38:32 +0100

    Added sortListByAge function, overrided the tocompare method ,and implemented comparable into People class

--- src/Function.java
+++ src/Function.java
@@ -16,6 +16,33 @@ public class Function {
         }
         return null;
     }
+    public People getOldestPeopleByAge(ArrayList<People> PeopleList) {
+        People oldestPerson = PeopleList.get(0);
+        for (int i = 1; i < PeopleList.size(); i++) {
+            if (PeopleList.get(i).compareTo(oldestPerson) > 0) {
+                oldestPerson = PeopleList.get(i);
+            }
+        }
+        return oldestPerson;
+    }
+
+    public int getAverageAge(ArrayList<People> PeopleList) {
+        int sum = 0;
+        for (int i = 0; i < PeopleList.size(); i++) {
+            sum += PeopleList.get(i).getAge();
+        }
+        return sum / PeopleList.size();
+    }
+
+    public ArrayList<People> sortListByAge(ArrayList<People> peopleList) {
+        peopleList.sort(People::compareTo);
+        return peopleList;
+    }
+
+
+
+
+
 
 
 }
