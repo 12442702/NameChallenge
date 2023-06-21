9321b3e19335759a574b60ba487cbea2195cd477
Peter <LiX196@cardiff.ac.uk>
2023-06-21 10:44:49 +0100

    Created Menu function ,combine all the function into the menu ,the app can work now

--- src/Function.java
+++ src/Function.java
@@ -16,7 +16,7 @@ public class Function {
         }
         return null;
     }
-    public People getOldestPeopleByAge(ArrayList<People> PeopleList) {
+    public People getOldestPeople(ArrayList<People> PeopleList) {
         People oldestPerson = PeopleList.get(0);
         for (int i = 1; i < PeopleList.size(); i++) {
             if (PeopleList.get(i).compareTo(oldestPerson) > 0) {
@@ -24,6 +24,16 @@ public class Function {
             }
         }
         return oldestPerson;
+    }
+
+    public People getYoungestPeople (ArrayList<People> PeopleList) {
+        People youngestPerson = PeopleList.get(0);
+        for (int i = 1; i < PeopleList.size(); i++) {
+            if (PeopleList.get(i).compareTo(youngestPerson) < 0) {
+                youngestPerson = PeopleList.get(i);
+            }
+        }
+        return youngestPerson;
     }
 
     public int getAverageAge(ArrayList<People> PeopleList) {
@@ -37,6 +47,78 @@ public class Function {
     public ArrayList<People> sortListByAge(ArrayList<People> peopleList) {
         peopleList.sort(People::compareTo);
         return peopleList;
+    }
+
+    public void menu() {
+        Scanner input = new Scanner(System.in);
+        boolean exit = false;
+        People person1 = new People("Alice", 20);
+        People person2 = new People("Bob", 25);
+        People person3 = new People("Carol  ",  30);
+        People person4 = new People("Dave", 35);
+        ArrayList<People> peopleList = new ArrayList<People>();
+        peopleList.add(person1);
+        peopleList.add(person2);
+        peopleList.add(person3);
+        peopleList.add(person4);
+        do {
+            System.out.println("0. Add a person");
+            System.out.println("1. Find a person");
+            System.out.println("2. Get youngest people");
+            System.out.println("3. Get average age");
+            System.out.println("4. Get oldest people");
+            System.out.println("5. Sort list by age");
+            System.out.println("6. Print list");
+            System.out.println("7. Exit");
+            System.out.println("Enter your choice: ");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 0:
+                    System.out.println("Enter name: ");
+                    String name = input.next();
+                    System.out.println("Enter age: ");
+                    int age = input.nextInt();
+                    peopleList = addPeople(peopleList, name, age);
+                    break;
+                case 1:
+                    System.out.println("Enter name: ");
+                    String name2 = input.next();
+                    People person = findPerson(peopleList, name2);
+                    if (person != null) {
+                        System.out.println("Name: " + person.getName() + " Age: " + person.getAge());
+                    } else {
+                        System.out.println("Person not found");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Youngest " + getYoungestPeople(peopleList));
+                    break;
+                case 3:
+                    System.out.println("Average age is: " + getAverageAge(peopleList));
+                    break;
+                case 4:
+                    System.out.println("Oldest " + getOldestPeople(peopleList));
+                    break;
+                case 5:
+                    ArrayList<People> sortedList = sortListByAge(peopleList);
+                    for (int i = 0; i < sortedList.size(); i++) {
+                        System.out.println("Name: " + sortedList.get(i).getName() + " Age: " + sortedList.get(i).getAge());
+                    }
+                    break;
+                case 6:
+                    for (int i = 0; i < peopleList.size(); i++) {
+                        System.out.println("Name: " + peopleList.get(i).getName() + " Age: " + peopleList.get(i).getAge());
+                    }
+
+                    break;
+                case 7:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+        } while (!exit);
     }
 
 
